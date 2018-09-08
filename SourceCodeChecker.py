470c0b5f523322258ac6ca2fb2c85b5b490384d1
Gabor Vizi <fasten90@gmail.com>
2018-09-08 22:26:27 +0200

    # CodeRefactor: Made class for FileAnalysis

--- SourceCodeChecker.py
+++ SourceCodeChecker.py
@@ -4,129 +4,158 @@ import glob
 import codecs
 
 
-# TODO: Make a config for set these
-__CONFIG_ENCODE = "utf8"
+class FileIssue():
+    def __init__(self, file_path, line_number, issue):
+        self.__file_path = file_path
+        self.__line_number = line_number
+        self.__issue = issue
 
-__CONFIG_TABS_ENABLED = False
+    def print_issue(self):
+        print("{} file, {}. line has issue: {}".format(
+            self.__file_path,
+            self.__line_number,
+            self.__issue
+            ))
 
-__CONFIG_ASCII_CHECKER = False
 
-__CONFIG_NEWLINE_CHARS = "\r\n"
+class FileAnalysis():
 
-__CONFIG_ENABLED_INDENT_NUM = 4
+    # TODO: Make a config for set these
+    __CONFIG_ENCODE = "utf8"
+    
+    __CONFIG_TABS_ENABLED = False
+    
+    __CONFIG_ASCII_CHECKER = False
+    
+    __CONFIG_NEWLINE_CHARS = "\r\n"
+    
+    __CONFIG_ENABLED_INDENT_NUM = 4
+
+
+    def __init__(self, file_path):
+        """ Read the file """
+        
+        self.__file_path = file_path
+        self.__issues = []
+        self.__new_file = []
+        self.__file = []
+
+        print("Check file: {}".format(file_path))
+
+        #file = open(file_path,'rt')
+        file = codecs.open(file_path, 'r', encoding=self.__CONFIG_ENCODE)
+
+        # Read entire file
+        try:
+            # This is an check for ENCODE
+            self.__file = file.readlines()
+            #print("File encode is OK")
+        except:
+            self.add_issue(0, "Not {} encoded".format(self.__CONFIG_ENCODE))
+
+        file.close()
+
+
+    def analyze(self):
+
+        if self.__CONFIG_ASCII_CHECKER:
+            self.check_ASCII()
+    
+        self.check_newline()
+    
+        if not self.__CONFIG_TABS_ENABLED:
+            self.check_tabs()
+    
+        if not self.__CONFIG_TABS_ENABLED:
+            self.check_indent()
+    
+        self.check_trailing_whitespace()
+        
+    
+    def add_issue(self, line_number, issue_text):
+        self.__issues.append(FileIssue(self.__file_path,
+                                    line_number,
+                                    issue_text))
+
+
+    def print_issues(self):
+        for issue in self.__issues:
+            issue.print_issue()
+
+
+    def check_ASCII(self):
+        for i, line in enumerate(self.__file):
+            for char in line:
+                if char > 127:
+                    self.add_issue(i, "There is a non-ASCII character!")
+                    return False    
+        return True
+    
+    
+    def check_newline(self):
+        # Check every line has good newline? (and the last line too)
+        for i, line in enumerate(self.__file):
+            # Get last characters
+            last_chars = line[-len(self.__CONFIG_NEWLINE_CHARS) : ]
+            if last_chars != self.__CONFIG_NEWLINE_CHARS:
+                self.add_issue(i, "There is a wrong newline in the file!")
+                return False
+        return True
+    
+    
+    def correct_newline(self):
+        # TODO: Imlement it. Shall rewrite the file, or only replace?
+        pass
+    
+    
+    def check_tabs(self):
+        for i, line in enumerate(self.__file):
+            if "\t" in line:
+                self.add_issue(i, "There is a tabulator in the file!")
+                return False
+        return True
+    
+    
+    def correct_tabs(self):
+        pass
+        # TODO: Implement:
+        # 1. replace tabs --> spaces
+        # 2. replace spaces --> tab, but only in leading
+    
+    
+    def check_trailing_whitespace(self):
+        for i, line in enumerate(self.__file):
+            # Strip newline characters
+            line = line.rstrip(self.__CONFIG_NEWLINE_CHARS)
+            if line != line.rstrip():
+                self.add_issue(i, "There is trailing whitespace!")
+                return False
+        return True
+
+    
+    def check_indent(self):
+        # Indent is only interested in space-indented mode, not in tab mode
+        for i, line in enumerate(self.__file):
+            length_of_leading_spaces = len(line) - len(line.lstrip(' '))
+            if (length_of_leading_spaces % self.__CONFIG_ENABLED_INDENT_NUM) != 0:
+                self.add_issue(i, "Indent is wrong!")
+                return False
+        return True
 
 
 def main(dir_path=".", dir_relative=True, file_types="*.c", checks=[], change_mode=False):
     print("Directory: {}\n" \
           "File types: {}".format(
               dir_path, file_types))
-    
 
     # Walk directories
     file_list = glob.glob(dir_path + "\\" + file_types)
-    
+
+    # Check files
     for file_path in file_list:
-        check_file(file_path)
-
-
-def check_file(file_path):
-    """ Check the file """
-    print("Check file: {}".format(file_path))
-    #file = open(file_path,'rt')
-    file = codecs.open(file_path, 'r', encoding=__CONFIG_ENCODE)
-
-    check_encode(file)
-    
-    if __CONFIG_ASCII_CHECKER:
-        check_ASCII(file)
-
-    file.seek(0)
-    check_newline(file)
-
-    file.seek(0)
-    if not __CONFIG_TABS_ENABLED:
-        check_tabs(file)
-
-    file.seek(0)
-    if not __CONFIG_TABS_ENABLED:
-        check_indent(file)
-
-    file.seek(0)
-    check_trailing_whitespace(file)
-    
-    file.close()
-    
-    
-def check_encode(file):
-    try:
-        #file.read().decode(__CONFIG_ENCODE)
-        file.readlines()
-        file.seek(0)
-        #print("File encode is OK")
-        return True
-    except:
-        print("File corrupt! Not {} encoded".format(__CONFIG_ENCODE))
-        return False
-
-
-def check_ASCII(file):
-    for line in file.readlines():
-        for char in line:
-            if char > 127:
-                print("File corrupt! There is a non-ASCII character!")
-                return False    
-    return True
-
-
-def check_newline(file):
-    # Check every line has good newline? (and the last line too)
-    for line in file.readlines():
-        # Get last characters
-        last_chars = line[-len(__CONFIG_NEWLINE_CHARS) : ]
-        if last_chars != __CONFIG_NEWLINE_CHARS:
-            print("File corrupt! There is a wrong newline in the file!")
-            return False
-    return True
-
-
-def correct_newline(file):
-    # TODO: Imlement it. Shall rewrite the file, or only replace?
-    pass
-
-
-def check_tabs(file):
-    for line in file.readlines():
-        if "\t" in line:
-            print("File corrupt! There is a tabulator in the file!")
-            return False
-    return True
-
-
-def correct_tabs(file):
-    pass
-    # TODO: Implement:
-    # 1. replace tabs --> spaces
-    # 2. replace spaces --> tab, but only in leading
-
-
-def check_trailing_whitespace(file):
-    for line in file.readlines():
-        # Strip newline characters
-        line = line.rstrip(__CONFIG_NEWLINE_CHARS)
-        if line != line.rstrip():
-            print("File problem! There is trailing whitespace!")
-            return False
-    return True
-
-
-def check_indent(file):
-    # Indent is only interested in space-indented mode, not in tab mode
-    for line in file.readlines():
-        length_of_leading_spaces = len(line) - len(line.lstrip(' '))
-        if (length_of_leading_spaces % __CONFIG_ENABLED_INDENT_NUM) != 0:
-            print("File problem! Indent is wrong!")
-            return False
-    return True
+        file_analysis = FileAnalysis(file_path)
+        file_analysis.analyze()
+        file_analysis.print_issues()
 
 
 if __name__ == "__main__":
