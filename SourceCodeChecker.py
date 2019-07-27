5f6308a89749d47dbc0632b9741c582c7b69aff1
Gabor Vizi <fasten90@gmail.com>
2019-07-27 23:38:35 +0200

    # Temporary: checker run for FastenMachine - REVERT later, it is not acceptable solution

--- SourceCodeChecker.py
+++ SourceCodeChecker.py
@@ -818,9 +818,9 @@ if __name__ == "__main__":
     # execute only if run as a script
     # Test:
     # FileAnalysis(file_path=None)
-    run_checker(dir_path="Src\\**", dir_relative=True, recursive=True)
-    run_checker(dir_path="Inc\\**", dir_relative=True, recursive=True)
-    #run_checker(dir_path="Drivers\\x86\\**", dir_relative=True, recursive=True)
+    run_checker(dir_path="..\\..\AtollicWorkspace\\FastenHomeAut\\Src\\**", dir_relative=True, recursive=True)
+    run_checker(dir_path="..\\..\AtollicWorkspace\\FastenHomeAut\\Inc\\**", dir_relative=True, recursive=True)
+    # run_checker(dir_path="Drivers\\x86\\**", dir_relative=True, recursive=True)
 
 # TODO: Unittest for TAB
 # TODO: Unittest for not tab (indent!)
